c840ba7e95184158ebee8bfd485622937041bb0c
Mike Davies <isofarro2@gmail.com>
2013-10-20 20:12:13 +0100

    Create phpunit test bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+# SQLite databases for testing 
+define('UNITTEST_DB_PATH', dirname(dirname(__FILE__)) . '/var/unittest-db/');
+
+# Use composer's autoloader
+require 'vendor/autoload.php';
+
+?>
