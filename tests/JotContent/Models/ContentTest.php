502f21efcbabefc6edd053753c63f3a44f4b116f
Mike Davies <isofarro2@gmail.com>
2013-10-23 21:05:26 +0100

    Check property validity

--- tests/JotContent/Models/ContentTest.php
+++ tests/JotContent/Models/ContentTest.php
@@ -12,6 +12,22 @@ class ContentTest extends PHPUnit_Framework_TestCase {
     public function testClassExists() {
         $this->assertTrue(class_exists('JotContent\Models\Content'));
         $this->assertTrue(is_a($this->content, 'JotContent\Models\Content'));
+        $this->assertTrue(is_a($this->content, 'JotContent\Model'));
+
+        $this->assertTrue(property_exists($this->content, '_id'));
+
+        $this->assertTrue(property_exists($this->content, '_content_model_id'));
+        $this->assertTrue(property_exists($this->content, '_content_id'));
+
+        $this->assertTrue(property_exists($this->content, 'slug'));
+        $this->assertTrue(property_exists($this->content, 'title'));
+        $this->assertTrue(property_exists($this->content, 'summary'));
+        $this->assertTrue(property_exists($this->content, 'thumbnail'));
+
+        $this->assertTrue(property_exists($this->content, 'contentType'));
+        $this->assertTrue(property_exists($this->content, 'dateAdded'));
+        $this->assertTrue(property_exists($this->content, 'lastUpdated'));
+        $this->assertTrue(property_exists($this->content, 'status'));
     }
 
 
