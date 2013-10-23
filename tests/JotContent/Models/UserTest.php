f883bbacd8711d801f8e71853d75bfb039970740
Mike Davies <isofarro2@gmail.com>
2013-10-23 21:08:51 +0100

    Check user model properties

--- tests/JotContent/Models/UserTest.php
+++ tests/JotContent/Models/UserTest.php
@@ -12,6 +12,17 @@ class UserTest extends PHPUnit_Framework_TestCase {
     public function testClassExists() {
         $this->assertTrue(class_exists('JotContent\Models\User'));
         $this->assertTrue(is_a($this->user, 'JotContent\Models\User')); 
+        $this->assertTrue(is_a($this->user, 'JotContent\Model')); 
+
+        $this->assertTrue(property_exists($this->user, '_id'));
+        $this->assertTrue(property_exists($this->user, 'name'));
+        $this->assertTrue(property_exists($this->user, 'email'));
+        $this->assertTrue(property_exists($this->user, 'avatar'));
+        $this->assertTrue(property_exists($this->user, 'passwordHash'));
+        $this->assertTrue(property_exists($this->user, 'status'));
+        $this->assertTrue(property_exists($this->user, 'dateAdded'));
+        $this->assertTrue(property_exists($this->user, '_sessions'));
+        $this->assertTrue(property_exists($this->user, '_roles'));
     }
 
 
