275461caa00bef2a561e11660d76c08790d1d13e
Mike Davies <isofarro2@gmail.com>
2013-10-25 07:41:06 +0100

    Switch to returning a PDO datasource, rather than a DSN to one

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,22 @@
 <?php
+
+use JotContent\DataSources\PdoDataSource;
+
 # SQLite databases for testing 
 define('UNITTEST_DB_PATH', dirname(dirname(__FILE__)) . '/var/unittest-db/');
 
 # Use composer's autoloader
 require 'vendor/autoload.php';
 
+
 class UnitTestUtils {
     static $baseDsnPath = UNITTEST_DB_PATH;
 
     public static function getDatasource($name=NULL) {
-        $dsn = 'sqlite:' . self::$baseDsnPath . 'entity-' . $name . '.db';
+        $dsnSource = 'sqlite:' . self::$baseDsnPath . 'entity-' . $name . '.db';
 
-        return $dsn;
+        $dataSource = new PdoDataSource($dsnSource);
+        return $dataSource;
     }
 
 }
