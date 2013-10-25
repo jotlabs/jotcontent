57012caf1f4f1243959b9f390b9ead8919a52d81
Mike Davies <isofarro2@gmail.com>
2013-10-25 07:48:01 +0100

    Move utilities object out of bootstrap into it's own utilities class in JotContent\Testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,9 @@
 <?php
 
-use JotContent\DataSources\PdoDataSource;
-
 # SQLite databases for testing 
 define('UNITTEST_DB_PATH', dirname(dirname(__FILE__)) . '/var/unittest-db/');
 
 # Use composer's autoloader
 require 'vendor/autoload.php';
-
-
-class UnitTestUtils {
-    static $baseDsnPath = UNITTEST_DB_PATH;
-
-    public static function getDatasource($name=NULL) {
-        $dsnSource = 'sqlite:' . self::$baseDsnPath . 'entity-' . $name . '.db';
-
-        $dataSource = new PdoDataSource($dsnSource);
-        return $dataSource;
-    }
-
-}
 
 ?>
