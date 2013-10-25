7b1e2540aaf8c1e11a0192f8aebf4f68ee8d3700
Mike Davies <isofarro2@gmail.com>
2013-10-25 07:33:38 +0100

    Utility method to abstract away datasource setup so we can later replace with an in-memory SQLite database

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,15 @@ define('UNITTEST_DB_PATH', dirname(dirname(__FILE__)) . '/var/unittest-db/');
 # Use composer's autoloader
 require 'vendor/autoload.php';
 
+class UnitTestUtils {
+    static $baseDsnPath = UNITTEST_DB_PATH;
+
+    public static function getDatasource($name=NULL) {
+        $dsn = 'sqlite:' . self::$baseDsnPath . 'entity-' . $name . '.db';
+
+        return $dsn;
+    }
+
+}
+
 ?>
